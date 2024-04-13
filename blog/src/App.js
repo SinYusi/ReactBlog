cc246cf2ea62918ca365c9d532225f60c64cfb5b
SinYusi <yusi4781@naver.com>
2024-04-13 22:52:10 +0900

    Lesson_5

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
@@ -10,6 +12,7 @@ function App() {
 
   let [logo,setLogo] = useState('ReactBlog');
   let [글제목,a] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
@@ -17,7 +20,7 @@ function App() {
         <h4 style = {{color : 'red', fontSize : '16px'}}>{logo}</h4>
       </div>
       <div className="list">
-        <h4>{글제목[0]}</h4>
+        <h4>{글제목[0]} <span onClick = {() => { 따봉변경(따봉+1)}}>👍</span> { 따봉 } </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
