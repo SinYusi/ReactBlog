6ce26cc0ebfc1236841adbc04b15cdb9325d6fbe
SinYusi <yusi4781@naver.com>
2024-04-14 01:54:55 +0900

    Lecture 6

--- blog/src/App.js
+++ blog/src/App.js
@@ -11,7 +11,7 @@ function App() {
   //document.querySelector('h4').innerHTML = post;
 
   let [logo,setLogo] = useState('ReactBlog');
-  let [글제목,a] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
 
   return (
@@ -19,6 +19,13 @@ function App() {
       <div className="black-nav">
         <h4 style = {{color : 'red', fontSize : '16px'}}>{logo}</h4>
       </div>
+
+      <button onClick={()=>{
+        let copy=[...글제목];
+        copy[0]='여자 코트 추천'
+        글제목변경(copy);
+      }}>글수정</button>
+
       <div className="list">
         <h4>{글제목[0]} <span onClick = {() => { 따봉변경(따봉+1)}}>👍</span> { 따봉 } </h4>
         <p>2월 17일 발행</p>
