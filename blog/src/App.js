105ddcd9b876e88bce1d4c1221322980df09d9df
SinYusi <yusi4781@naver.com>
2024-04-16 01:29:19 +0900

    Lecture 8

--- blog/src/App.js
+++ blog/src/App.js
@@ -13,6 +13,7 @@ function App() {
   let [logo,setLogo] = useState('ReactBlog');
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
@@ -20,13 +21,12 @@ function App() {
       <div className="black-nav">
         <h4 style = {{color : 'red', fontSize : '16px'}}>{logo}</h4>
       </div>
-
+      
       <button onClick={()=>{
         let copy=[...글제목];
         copy.sort();
         글제목변경(copy)
       }}>정렬버튼</button>
-      
       <button onClick={()=>{
         let copy=[...글제목];
         copy[0]='여자 코트 추천'
@@ -42,11 +42,15 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4 onClick={()=>{
+          setModal(!modal)
+        }}>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
-      </div>\
+      </div>
 
-      <Modal/>
+      {
+        modal == true ? <Modal/> : null
+      }
 
     </div>
   );
