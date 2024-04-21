9ff451296720709185ad07cd1eb373198be812ab
SinYusi <yusi4781@naver.com>
2024-04-21 23:33:53 +0900

    Part2 : Lecture 3

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,7 +1,12 @@
 import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap';
 import './App.css';
+import { useState } from 'react';
+import data from './data.js';
 
 function App() {
+
+  let [restaurant] = useState(data)
+
   return (
     <div className="App">
       <Navbar expand="lg" className="bg-body-tertiary">
@@ -38,20 +43,20 @@ function App() {
         <div className="row">
           <div className="col-md-4">
             <img src={process.env.PUBLIC_URL + '/img/MealPlanB.jpg'} height={200} width={200} alt = "밀플랜비"/>
-            <h4>밀플랜비</h4>
-            <p>브리또</p>
+            <h4>{restaurant[0].title}</h4>
+            <p>{restaurant[0].content}</p>
           </div>
           
           <div className="col-md-4">
             <img src={process.env.PUBLIC_URL + '/img/GoGos.jpg'} height={200} width={200} alt="고고스"/>
-            <h4>고고스카페</h4>
-            <p>커피</p>
+            <h4>{restaurant[1].title}</h4>
+            <p>{restaurant[1].content}</p>
           </div>
 
           <div className="col-md-4">
             <img src={process.env.PUBLIC_URL + '/img/ChinaHouse.jpg'} height={200}width={200} alt='중국집'/>
-            <h4>중국집</h4>
-            <p>짜장면</p>
+            <h4>{restaurant[2].title}</h4>
+            <p>{restaurant[2].content}</p>
           </div>
         </div>
       </div>
---
+++ shop/src/data.js
@@ -0,0 +1,24 @@
+let data = [
+  {
+    id : 0,
+    title : "MealPlanB",
+    content : "브리또",
+    price : 6000
+  },
+
+  {
+    id : 1,
+    title : "GoGos",
+    content : "커피",
+    price : 4500
+  },
+
+  {
+    id : 2,
+    title : "ChinaHouse",
+    content : "짜장면",
+    price : 5000
+  }
+]
+
+export default data;
