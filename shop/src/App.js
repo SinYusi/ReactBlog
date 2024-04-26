d5176dc3bf09e40322617045a1f0ec077e56cd20
SinYusi <yusi4781@naver.com>
2024-04-26 18:20:50 +0900

    Part 2 : Lecture 5
    라우터를 사용하여 상세페이지를 만들었습니다.

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,6 +2,7 @@ import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import './App.css';
 import { useState } from 'react';
 import data from './data.js';
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
 
@@ -35,35 +36,75 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className="main-bg">
-        <input type='text' className='search'></input>
-      </div>
-      <div>
-        <h2>맛집 리스트</h2>
-      </div>
-      <div className="container">
-        <div className="row">
+      <Routes>
+        {
+          //홈페이지입니다.
+        }
+        <Route path="/" element={
+          <div>
+            <div className="main-bg">
+              <input type='text' className='search'></input>
+            </div>
+            <div>
+              <h2>맛집 리스트</h2>
+            </div>
+            <div className="container">
+              <div className="row">
+                {
+                  restaurant.map((a, i) => {
+                    return (
+                      <RestaurantList restaurant={restaurant} fileAddress={fileAddress} i={i} restaurant_name={restaurant_name}></RestaurantList>
+                    )
+                  })
+                }
 
-          {
-            restaurant.map((a, i) => {
-              return (
-                <RestaurantList restaurant={restaurant} fileAddress={fileAddress} i={i} restaurant_name={restaurant_name}></RestaurantList>
-              )
-            })
-          }
-          
-        </div>
-      </div>
+              </div>
+            </div>
+          </div>
+        } />
+
+        {
+          //디테일 페이지입니다.
+        }
+        {
+          restaurant.map((a, i) => {
+            return (
+              <Route path={'/detail' + restaurant[i].title} element={
+                <div className="container">
+                  <div className="row">
+                    <div className="col-md-6">
+                      <img src={fileAddress[i]} width="100%" />
+                    </div>
+                    <div className="col-md-6">
+                      <h4 className="pt-5">{restaurant[i].title}</h4>
+                      <p>{restaurant[i].ment}</p>
+                      <p>{restaurant[i].price}</p>
+                      <button className="btn btn-danger">주문하기</button>
+                      <Link to="/">홈으로 가기</Link>
+                    </div>
+                  </div>
+                </div>
+              }
+              ></Route>
+            )
+          })
+        }
+      </Routes>
+
     </div>
   );
 }
-
+{
+  //식당리스트 카드입니다.
+}
 function RestaurantList(props) {
   return (
     <div className="col-md-4">
-      <img src={process.env.PUBLIC_URL + props.fileAddress[props.i]} height={200} width={200} alt={props.restaurant_name} />
-      <h4>{props.restaurant[props.i].title}</h4>
-      <p>{props.restaurant[props.i].content}</p>
+      <Link to={'detail' + props.restaurant[props.i].title} style={{ textDecorationLine: 'none' }}>
+        <img src={process.env.PUBLIC_URL + props.fileAddress[props.i]} height={200} width={200} alt={props.restaurant_name} />
+        <h4 style={{ color: 'black' }}>{props.restaurant[props.i].title}</h4>
+        <p style={{ color: 'black' }}>{props.restaurant[props.i].content}</p>
+      </Link>
     </div>
   )
 }
--- shop/src/data.js
+++ shop/src/data.js
@@ -3,21 +3,24 @@ let data = [
     id : 0,
     title : "MealPlanB",
     content : "브리또",
-    price : 6000
+    price : 6000,
+    ment : "소스 가득한 브리또!"
   },
 
   {
     id : 1,
     title : "GoGos",
     content : "커피",
-    price : 4500
+    price : 4500,
+    ment : '스타벅스급 커피!'
   },
 
   {
     id : 2,
     title : "ChinaHouse",
     content : "짜장면",
-    price : 5000
+    price : 5000,
+    ment : '중국에 온 느낌을 주는 짜장면!'
   }
 ]
 
