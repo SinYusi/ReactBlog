ac7abbb3f4cb07e713fcec6740e8a11ecacdada6
SinYusi <yusi4781@naver.com>
2024-04-28 20:16:55 +0900

    Part 2 : Lecture 8
    styled-components 실습해보기

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,12 +1,22 @@
 /* eslint-disable */
 
 import { useParams } from "react-router-dom"
+import styled from "styled-components"
+
+//styled-components 실습
+// let YellowBtn = styled.button`
+//   background : ${props => props.bg};
+//   color : ${ props=>props.bg == 'blue' ? 'white' : 'black'}
+//   padding : 10px;
+// `
 
 function Detail(props) {
   let { id } = useParams();
-  let curData = props.restaurant.find((item) => {return(item.id == id)})
+  let curData = props.restaurant.find((item) => { return (item.id == id) })
   return (
     <div className="container">
+      {/* <YellowBtn bg='blue'>버튼</YellowBtn> */}
+
       <div className="row">
         <div className="col-md-6">
           <img src={process.env.PUBLIC_URL + '/img/' + curData.title + '.jpg'} width="100%" alt={curData.title} />
