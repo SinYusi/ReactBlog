24ff51c4b075dbca00368fb3e9f8b29eb56a0120
SinYusi <yusi4781@naver.com>
2024-05-16 00:31:12 +0900

    Part 2 : Lecture 12
    ajax 통신 응용

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
-import styled from "styled-components"
+import { Nav } from "react-bootstrap"
 
 //styled-components 실습
 // let YellowBtn = styled.button`
@@ -15,10 +15,11 @@ function Detail(props) {
 
   let { id } = useParams();
   let curData = props.restaurant.find((item) => { return (item.id == id) })
-  let [count,setCount] = useState(0);
+  let [count, setCount] = useState(0);
   let [alert, setAlert] = useState(true);
   let [num, setNum] = useState('');
   let [textAlert, settextAlert] = useState(false);
+  let [tab, setTab] = useState(0);
 
   useEffect(() => {
     let a = setTimeout(() => { setAlert(false) }, 2000)
@@ -28,7 +29,7 @@ function Detail(props) {
   }, [])
 
   useEffect(() => {
-    if(isNaN(num) == true){
+    if (isNaN(num) == true) {
       settextAlert(true);
     }
   })
@@ -44,19 +45,46 @@ function Detail(props) {
 
       <div className="row">
         <div className="col-md-6">
-          <img src={process.env.PUBLIC_URL + '/img/' + curData.title + '.jpg'} width="100%" alt={curData.title} />
+          <img src={process.env.PUBLIC_URL + '/img/' + curData.title + '.jpg'} width="50%" alt={curData.title} />
         </div>
         <div className="col-md-6">
           {
             textAlert == true ? <p>숫자만 적으셈</p> : null
           }
-          <input type="text" onChange={(e)=>{setNum(e.target.value)}}></input>
+          <input type="text" onChange={(e) => { setNum(e.target.value) }}></input>
           <h4 className="pt-5">{curData.title}</h4>
           <p>{curData.ment}</p>
           <p>{curData.price}</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+
+      <Nav variant="tabs" defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link eventKey="link0" onClick={() => { setTab(0) }}>버튼0</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link1" onClick={() => { setTab(1) }}>버튼1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link2" onClick={() => { setTab(2) }}>버튼2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <TabContent tab={tab} />
+    </div>
+  )
+}
+
+function TabContent({ tab }) {
+  // if (props.tab == 0)
+  //   return (<div>내용 0</div>)
+  // if (props.tab == 1)
+  //   return (<div>내용 1</div>)
+  // if (props.tab == 2)
+  //   return (<div>내용 2</div>)
+  return (
+    <div className="start end">
+      {[<div>내용 0</div>, <div>내용 1</div>, <div>내용 2</div>][tab]}
     </div>
   )
 }
